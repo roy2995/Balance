486ee8b0874e2daac785a788bb140db5547619af
Flasf <abdielr507@gmail.com>
2022-10-23 23:43:29 -0500

    Animaciones corregidas
    y animaciones hacia atrás añadidas

--- Balance/Assets/Scripts/Player/animationStateController.cs
+++ Balance/Assets/Scripts/Player/animationStateController.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class animationStateController : MonoBehaviour
 {
     Animator animator;
-    int isWalkingHash, isRunningHash;
+    int isWalkingHash, isRunningHash, isWalkingBackwardsHash;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         isWalkingHash = Animator.StringToHash("isWalking");
+        isWalkingBackwardsHash = Animator.StringToHash("isWalkingBackwards");
         isRunningHash = Animator.StringToHash("isRunning");
     }
 
@@ -19,31 +20,42 @@ public class animationStateController : MonoBehaviour
     void Update()
     {
         bool bIsForwardPressed = Input.GetKey(KeyCode.W);
+        bool bIsBackwardsPressed = Input.GetKey(KeyCode.S);
         bool bIsRunPressed = Input.GetKey(KeyCode.LeftShift);
 
         //performance
         bool bIsWalking = animator.GetBool(isWalkingHash);
         bool bIsRunning = animator.GetBool(isRunningHash);
+        bool bIsWalkingBackwards = animator.GetBool(isWalkingBackwardsHash);
         
-
+        //de idle a caminar
         if (!bIsWalking && bIsForwardPressed)
         {
             animator.SetBool(isWalkingHash,true);
         }
-
+        //de caminar a idle
         if (bIsWalking && !bIsForwardPressed)
         {
             animator.SetBool(isWalkingHash, false);
         }
-
+        //si no está corriendo, se presiona W y shift izq (para correr)
         if(!bIsRunning && (bIsForwardPressed && bIsRunPressed))
         {
             animator.SetBool(isRunningHash,true);
         }
-
+        //si está corriendo y se deja de caminar (presionar W) o si se deja de correr
         if(bIsRunning && (!bIsForwardPressed || !bIsRunPressed))
         {
             animator.SetBool(isRunningHash, false);
+        }
+
+        if (!bIsWalkingBackwards && bIsBackwardsPressed)
+        {
+            animator.SetBool(isWalkingBackwardsHash,true);
+        }
+        if (bIsWalkingBackwards && !bIsBackwardsPressed)
+        {
+            animator.SetBool(isWalkingBackwardsHash, false);
         }
     }
 }
